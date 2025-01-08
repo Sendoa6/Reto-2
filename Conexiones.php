446c76cb739c2f23a609c0bce6ff3fe56e57e875
Sendoa6 <sendoa.perez06@somo.eus>
2025-01-08 13:17:32 +0100

    Inicio registro frmphp

--- Conexiones.php
+++ Conexiones.php
@@ -1,24 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Conexiones</title>
-</head>
-<body>
-<?php
-$host = 'localhost'; // Cambia según tu servidor
-$dbname = 'mi_base_datos';
-$user = 'root'; // Usuario de tu base de datos
-$password = ''; // Contraseña del usuario
+<?php 
 
-try {
-    $conexion = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error de conexión: " . $e->getMessage());
+function f_sql($peticion){
+    
 }
-?>
 
-</body>
-</html>
+
+?>
---
+++ RegistroFRM.php
@@ -0,0 +1,23 @@
+<?php 
+$dni = $_POST['dni'];
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$telefono = $_POST['telefono'];
+$Correo = $_POST['Correo'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$password2 = $_POST['password2'];
+$NumeroSS = $_POST['NumeroSS'];
+
+$id_usuario = $_POST['id_usuario'];
+
+FOR ($I = 0; $I <= 99999; $I++){
+    
+}
+
+$peticion = "INSERT INTO";
+
+$respuesta = f_sql($peticion);
+
+
+?>
