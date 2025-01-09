c4890f40a3a94fbff1393ac1c83b290cf65aebcc
Miikeel16 <mikel.garcia06@somo.eus>
2025-01-09 11:14:44 +0100

    php

---
+++ PruebasMikel.php
@@ -0,0 +1,9 @@
+<?php
+if ($password2 != $password){
+    echo "Las contraseñas no son iguales";
+}
+if (!is_numeric($telefono) && strlen($telefono) != 9) {
+    echo "El número de teléfono no es válido";
+}
+if ($dni )
+?>
