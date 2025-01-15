49bc639bec65d143970f5d5ac47f92fa54fef007
Sendoa6 <sendoa.perez06@somo.eus>
2025-01-15 08:15:01 +0100

    cambios

--- registro_libros.php
+++ registro_libros.php
@@ -3,6 +3,13 @@
     session_start();
     include 'Conexiones.php';
 
-    
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $telefono = $_POST["telefono"];
+    $Correo = $_POST["Correo"];
+    $username = $_POST["username"];
+
+
+    $query = "INSERT INTO usuarios(nombre,apellidos,telefono,correo,nombre_usuario,contrasena,contrasena2,NumeroSS) VALUES ('$nombre','$apellido','$telefono','$Correo','$username','$password','$password2','$NumeroSS')";
 
 ?>
